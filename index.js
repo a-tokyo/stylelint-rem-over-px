47bf187ac01ecdc213a85a71c69382216d44af17
Tokyo <ahmed.tokyo1@gmail.com>
2023-01-28 16:00:52 +0000

    :zap: enhance docs

--- index.js
+++ index.js
@@ -3,24 +3,33 @@
 const stylelint = require('stylelint');
 const valueParser = require('postcss-value-parser');
 
-/** rule name */
-const ruleName = 'rem-over-px/rem-over-px';
+/** plugin name - prefixes all rules as per stylelint requirements */
+const PLUGIN_NAME = 'rem-over-px';
+
+/** rule name for rem-over-px */
+const ruleName = `${PLUGIN_NAME}/rem-over-px`;
 
 /** rule messages */
 const messages = stylelint.utils.ruleMessages(ruleName, {
-  rem(val = '') {
+  /** Report message for prefer rem over px */
+  remOverPx(val = '') {
     return `Expected px unit in "${val}" to be rem.`;
   },
 });
 
 /** default secondary options */
 const defaultSecondaryOptions = {
+  /** properties to ignore */
   ignore: ['1px'],
+  /** functions to ignore */
   ignoreFunctions: ['url'],
+  /** @ rules to ignore */
   ignoreAtRules: ['media'],
+  /** Base font size - used by autofix to convert px to rem */
   fontSize: 16,
 };
 
+/** Regex to match pixels declarations in a string */
 // eslint-disable-next-line
 const regexPX = new RegExp(/(\d+\.?\d*)px/, 'g');
 
@@ -32,6 +41,7 @@ const _pxToRem = (CSSString = '', fontSize) =>
 const _propInIgnoreList = (prop, list) =>
   prop && list.some((item) => prop.indexOf(item) > -1);
 
+/** checks if prop is in ignore list with px */
 const _propAddXpxInIgnoreList = (prop, list, px) => {
   const reg = new RegExp(`\\s${px}`);
 
@@ -48,16 +58,21 @@ const _propAddXpxInIgnoreList = (prop, list, px) => {
  */
 const _hasForbiddenPX = (node, options) => {
   const { type } = node;
+  /** value to check */
   const value = type === 'decl' ? node.value : node.params;
+  /** prop to check */
   const prop = type === 'decl' ? node.prop : null;
 
+  /** parsed value */
   const parsed = valueParser(value);
+  /* parse secondaryOptions */
   const {
     ignore = defaultSecondaryOptions.ignore,
     ignoreFunctions = defaultSecondaryOptions.ignoreFunctions,
     ignoreAtRules = defaultSecondaryOptions.ignoreAtRules,
   } = options;
 
+  /** Whether we matched px declarations */
   let hasPX = false;
 
   /** early exit and ignore */
@@ -70,6 +85,7 @@ const _hasForbiddenPX = (node, options) => {
     return;
   }
 
+  /** Walk through the parsed tree and match - return boolean, true if we matched an issue */
   parsed.walk((currNode) => {
     // if currNode is `url(xxx)`, prevent the traversal
     if (
@@ -86,12 +102,15 @@ const _hasForbiddenPX = (node, options) => {
       currNode.type === 'word' &&
       (matched = currNode.value.match(/^([-,+]?\d+(\.\d+)?px)$/))
     ) {
+      /** matched px value. eg: '10px' */
       const px = matched[1];
 
+      /** handled 0px edge case */
       if (px === '0px') {
         return;
       }
 
+      /** check if prop is in ignore list, else -> set hasPX since an issue was matched */
       if (
         !_propAddXpxInIgnoreList(prop, ignore, px) &&
         ignore.indexOf(px) === -1
@@ -99,6 +118,7 @@ const _hasForbiddenPX = (node, options) => {
         hasPX = true;
       }
     } else if (
+      /* handle string case, eg: mixins etc... */
       currNode.type === 'string' &&
       /(@\{[\w-]+\})px\b/.test(currNode.value)
     ) {
@@ -118,13 +138,15 @@ const pluginHandler =
     context = {},
   ) =>
   (root, result) => {
+    /** no primary option was provided or null, rule is disabled */
     if (!primaryOption) {
       return;
     }
 
-    // check for declarations
+    /* check for declarations */
     root.walkDecls((declaration) => {
       if (_hasForbiddenPX(declaration, secondaryOptionObject)) {
+        /* handle fixing */
         if (context.fix) {
           // Apply fixes using PostCSS API
           declaration.value = _pxToRem(
@@ -136,12 +158,12 @@ const pluginHandler =
           return;
         }
 
-        // report a problem
+        /* handle reporting */
         stylelint.utils.report({
           ruleName,
           result,
           node: declaration,
-          message: messages.rem(declaration),
+          message: messages.remOverPx(declaration),
         });
       }
     });
@@ -149,6 +171,7 @@ const pluginHandler =
     // check for rules
     root.walkAtRules((atRule) => {
       if (_hasForbiddenPX(atRule, secondaryOptionObject)) {
+        /* handle fixing */
         if (context.fix) {
           // Apply fixes using PostCSS API
           atRule.value = _pxToRem(atRule.value, secondaryOptionObject.fontSize);
@@ -157,12 +180,12 @@ const pluginHandler =
           return;
         }
 
-        // report a problem
+        /* handle reporting */
         stylelint.utils.report({
           ruleName,
           result,
           node: atRule,
-          message: messages.rem(atRule),
+          message: messages.remOverPx(atRule),
         });
       }
     });
