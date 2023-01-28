90cb65d50464dfc813a4a76f49c1bfa85e3225ac
Tokyo <ahmed.tokyo1@gmail.com>
2023-01-28 15:52:31 +0000

    :zap: enhance code + docs

--- README.md
+++ README.md
@@ -9,9 +9,12 @@ A stylelint rule to enforce the usage of rem units over px units. It can also be
 <a href="https://twitter.com/intent/follow?screen_name=ahmad_tokyo"><img src="https://img.shields.io/twitter/follow/ahmad_tokyo.svg?label=Follow%20@ahmad_tokyo" alt="Follow @ahmad_tokyo"></img></a>
 
 ```css
-width: 10px; // error
+width: 8px; // error -> can be autofixed to width: 0.5rem;
 height: 1.5rem; // ok
 border: 1px solid #000000; // ok
+border: 2px solid #000000; // error -> can be autofixed to width: 0.125rem;
+@media (max-width: 768px) { display: none }; // ok
+background-image: url('https://exapmle.com?size=500pxX500px'); // ok
 ```
 
 ## Installation
@@ -35,11 +38,10 @@ Add it to your stylelint config
     "stylelint-rem-over-px"
   ],
   "rules": {
-    // ...
-    "rem-over-px/rem-over-px": [true, { "ignore": ["1px"] }],
-    // or just:
+    // Declare the rule
     "rem-over-px/rem-over-px": true,
-    // ...
+    // Declaring the rule with default values is equivalent to:
+    // "rem-over-px/rem-over-px": [true, { "ignore": "1px", "ignoreFunctions": ["url"] , "ignoreAtRules": ["media"], fontSize: 16 }],
   }
 }
 ```
@@ -52,60 +54,25 @@ ignore value check.
 
 Valid value of Item: `propertyName` | `'1px'` | `'${propertyName} 1px'`
 
+Default: ["1px"]
+
 ### ignoreFunctions: string[]
 
 ignore check for functions.
 
-### example(1) (the default options)
+Default: ["url"]
 
-```javascript
-// all 1px is ok
-"rem-over-px/rem-over-px": [true, { "ignore": ["1px"] }],
-```
+### ignoreAtRules: string[]
 
-```css
-@padding-base: 20px; // error
+ignore check for @ rules.
 
-.foo {
-  border-top: 1px solid #ccc; // ok
-  padding: 10px; // error
-  height: 1px; // ok
-  padding: @padding-base * 2;
-}
-```
+Default: ["media"]
 
-### example(2)
+### fontSize: number
 
-```javascript
-//  - all `1px` or `font` is ok
-//  - rem(Npx) is ok
-"rem-over-px/rem-over-px": [true, { "ignore": ["1px", "font"], "ignoreFunctions": ["rem"] }],
-```
+Base font size in pixels. Used to fix px values to rem.
 
-```css
-.foo {
-  border-top: 1px solid #ccc; // ok
-  height: 1px; // ok
-  font-size: 24px; // ok
-  padding: 10px; // error
-  width: calc(100% - 10px); // error
-  font-size: rem(10px); // ok
-}
-```
-
-### example(3)
-
-```javascript
-// only `border + 1px` is ok
-"rem-over-px/rem-over-px": [true, { "ignore": ["border 1px"] }],
-```
-
-```css
-.foo {
-  border-top: 1px solid #ccc; // ok
-  height: 1px; // error
-}
-```
+Default: 16
 
 ### AutoFixing
 This plugin supports auto-fixing. Simply run stylelint with the --fix option. The plugin will then replace all px occurrences with rem.
--- index.js
+++ index.js
@@ -8,15 +8,17 @@ const ruleName = 'rem-over-px/rem-over-px';
 
 /** rule messages */
 const messages = stylelint.utils.ruleMessages(ruleName, {
-	rem(val = '') {
-		return `Expected px unit in "${val}" to be rem.`;
-	},
+  rem(val = '') {
+    return `Expected px unit in "${val}" to be rem.`;
+  },
 });
 
 /** default secondary options */
 const defaultSecondaryOptions = {
-	ignore: ['1px'],
-	fontSize: 16,
+  ignore: ['1px'],
+  ignoreFunctions: ['url'],
+  ignoreAtRules: ['media'],
+  fontSize: 16,
 };
 
 // eslint-disable-next-line
@@ -24,132 +26,151 @@ const regexPX = new RegExp(/(\d+\.?\d*)px/, 'g');
 
 /** Converts a string with px units to rem */
 const _pxToRem = (CSSString = '', fontSize) =>
-	CSSString.replace(regexPX, (match, n) => `${n / fontSize}rem`);
+  CSSString.replace(regexPX, (match, n) => `${n / fontSize}rem`);
 
 /** checks if prop is in ignore list */
-const _propInIgnoreList = (prop, list) => (
-  prop &&
-  list.some((item) => prop.indexOf(item) > -1)
-  );
+const _propInIgnoreList = (prop, list) =>
+  prop && list.some((item) => prop.indexOf(item) > -1);
 
 const _propAddXpxInIgnoreList = (prop, list, px) => {
-	const reg = new RegExp(`\\s${px}`);
+  const reg = new RegExp(`\\s${px}`);
 
-	return (
-		prop &&
-		list.some((item) => reg.test(item) && prop.indexOf(item.replace(reg, '')) > -1)
-	);
+  return (
+    prop &&
+    list.some(
+      (item) => reg.test(item) && prop.indexOf(item.replace(reg, '')) > -1,
+    )
+  );
 };
 
 /**
  * check if a value has forbidden `px`
  */
 const _hasForbiddenPX = (node, options) => {
-	const { type } = node;
-	const value = type === 'decl' ? node.value : node.params;
-	const prop = type === 'decl' ? node.prop : null;
+  const { type } = node;
+  const value = type === 'decl' ? node.value : node.params;
+  const prop = type === 'decl' ? node.prop : null;
 
-	const parsed = valueParser(value);
-	const { ignore = defaultSecondaryOptions.ignore, ignoreFunctions = [] } = options;
+  const parsed = valueParser(value);
+  const {
+    ignore = defaultSecondaryOptions.ignore,
+    ignoreFunctions = defaultSecondaryOptions.ignoreFunctions,
+    ignoreAtRules = defaultSecondaryOptions.ignoreAtRules,
+  } = options;
 
   let hasPX = false;
 
   /** early exit and ignore */
-	if (
+  if (
     /* ignore media queries */
-    type === 'atrule' && node.name === 'media'
-    ||
+    (type === 'atrule' && ignoreAtRules.indexOf(node.name) !== -1) ||
     /* ignore declarations ignored by props */
-    type === 'decl' && _propInIgnoreList(node.prop, ignore)
+    (type === 'decl' && _propInIgnoreList(node.prop, ignore))
   ) {
     return;
   }
 
-	parsed.walk((currNode) => {
-		// if currNode is `url(xxx)`, prevent the traversal
-		if (
-			currNode.type === 'function' &&
-			(currNode.value === 'url' || ignoreFunctions.indexOf(currNode.value) > -1)
-		) {
-			return false;
-		}
+  parsed.walk((currNode) => {
+    // if currNode is `url(xxx)`, prevent the traversal
+    if (
+      currNode.type === 'function' &&
+      ignoreFunctions.indexOf(currNode.value) !== -1
+    ) {
+      return false;
+    }
 
     /** whether a px value was matched */
-		let matched;
+    let matched;
 
-		if (currNode.type === 'word' && (matched = currNode.value.match(/^([-,+]?\d+(\.\d+)?px)$/))) {
-			const px = matched[1];
+    if (
+      currNode.type === 'word' &&
+      (matched = currNode.value.match(/^([-,+]?\d+(\.\d+)?px)$/))
+    ) {
+      const px = matched[1];
 
-			if (px === '0px') {
-				return;
-			}
+      if (px === '0px') {
+        return;
+      }
 
-			if (!_propAddXpxInIgnoreList(prop, ignore, px) && ignore.indexOf(px) === -1) {
-				hasPX = true;
-			}
-		} else if (currNode.type === 'string' && /(@\{[\w-]+\})px\b/.test(currNode.value)) {
-			// eg. ~'@{width}px'
-			hasPX = true;
-		}
-	});
+      if (
+        !_propAddXpxInIgnoreList(prop, ignore, px) &&
+        ignore.indexOf(px) === -1
+      ) {
+        hasPX = true;
+      }
+    } else if (
+      currNode.type === 'string' &&
+      /(@\{[\w-]+\})px\b/.test(currNode.value)
+    ) {
+      // eg. ~'@{width}px'
+      hasPX = true;
+    }
+  });
 
-	return hasPX;
+  return hasPX;
 };
 
 /** rem-over-px plugin handler */
 const pluginHandler =
-	(primaryOption, secondaryOptionObject = defaultSecondaryOptions, context = {}) =>
-	(root, result) => {
-		if (!primaryOption) {
-			return;
-		}
+  (
+    primaryOption,
+    secondaryOptionObject = defaultSecondaryOptions,
+    context = {},
+  ) =>
+  (root, result) => {
+    if (!primaryOption) {
+      return;
+    }
 
     // check for declarations
-		root.walkDecls((declaration) => {
-			if (_hasForbiddenPX(declaration, secondaryOptionObject)) {
-				if (context.fix) {
-					// Apply fixes using PostCSS API
-					declaration.value = _pxToRem(declaration.value, secondaryOptionObject.fontSize);
+    root.walkDecls((declaration) => {
+      if (_hasForbiddenPX(declaration, secondaryOptionObject)) {
+        if (context.fix) {
+          // Apply fixes using PostCSS API
+          declaration.value = _pxToRem(
+            declaration.value,
+            secondaryOptionObject.fontSize,
+          );
 
-					// Return and don't report a problem
-					return;
-				}
+          // Return and don't report a problem
+          return;
+        }
 
         // report a problem
-				stylelint.utils.report({
-					ruleName,
-					result,
-					node: declaration,
-					message: messages.rem(declaration),
-				});
-			}
-		});
+        stylelint.utils.report({
+          ruleName,
+          result,
+          node: declaration,
+          message: messages.rem(declaration),
+        });
+      }
+    });
 
     // check for rules
-		root.walkAtRules((atRule) => {
-			if (_hasForbiddenPX(atRule, secondaryOptionObject)) {
-				if (context.fix) {
-					// Apply fixes using PostCSS API
-					atRule.value = _pxToRem(atRule.value, secondaryOptionObject.fontSize);
+    root.walkAtRules((atRule) => {
+      if (_hasForbiddenPX(atRule, secondaryOptionObject)) {
+        if (context.fix) {
+          // Apply fixes using PostCSS API
+          atRule.value = _pxToRem(atRule.value, secondaryOptionObject.fontSize);
 
-					// Return and don't report a problem
-					return;
-				}
+          // Return and don't report a problem
+          return;
+        }
 
         // report a problem
-				stylelint.utils.report({
-					ruleName,
-					result,
-					node: atRule,
-					message: messages.rem(atRule),
-				});
-			}
-		});
-	};
+        stylelint.utils.report({
+          ruleName,
+          result,
+          node: atRule,
+          message: messages.rem(atRule),
+        });
+      }
+    });
+  };
 
 /**
  * Stylelint plugin rem-over-px
- * 
+ *
  * Enforces the usage of rem units over px units.
  */
 module.exports = stylelint.createPlugin(ruleName, pluginHandler);
